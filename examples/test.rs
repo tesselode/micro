364e52480643ed3599a883bce89ad2685dc9aa5e
Andrew Minnich <aminnich3@gmail.com>
2022-01-10 22:22:45 -0500

    basic structure

--- examples/test.rs
+++ examples/test.rs
@@ -1,82 +1,38 @@
-use std::{error::Error, time::Duration};
+use std::error::Error;
 
-use glow::HasContext;
+use micro::Context;
+use sdl2::event::Event;
 
-const VERTEX_SHADER: &str = include_str!("vertex.glsl");
-const FRAGMENT_SHADER: &str = include_str!("fragment.glsl");
+struct Game {
+	alt_color: bool,
+}
+
+impl Game {
+	fn new() -> Self {
+		Self { alt_color: false }
+	}
+}
+
+impl micro::Game<()> for Game {
+	fn event(&mut self, _ctx: &mut Context, event: Event) -> Result<(), ()> {
+		if let Event::KeyDown { .. } = event {
+			self.alt_color = true;
+		}
+		Ok(())
+	}
+
+	fn draw(&mut self, ctx: &mut Context) -> Result<(), ()> {
+		if self.alt_color {
+			ctx.graphics().clear(0.3, 0.4, 0.5, 1.0);
+		} else {
+			ctx.graphics().clear(0.1, 0.2, 0.3, 1.0);
+		}
+		Ok(())
+	}
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-	let sdl_context = sdl2::init()?;
-	let video_subsystem = sdl_context.video()?;
-	let gl_attr = video_subsystem.gl_attr();
-	gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
-	gl_attr.set_context_version(3, 3);
-	let window = video_subsystem
-		.window("Window", 800, 600)
-		.opengl()
-		.build()?;
-	let _ctx = window.gl_create_context()?;
-	let gl_ctx = unsafe {
-		glow::Context::from_loader_function(|name| {
-			video_subsystem.gl_get_proc_address(name) as *const _
-		})
-	};
-	let mut event_pump = sdl_context.event_pump()?;
-	let shader_program;
-	let vertex_array;
-	unsafe {
-		// set up shaders
-		let vertex_shader = gl_ctx.create_shader(glow::VERTEX_SHADER)?;
-		gl_ctx.shader_source(vertex_shader, VERTEX_SHADER);
-		gl_ctx.compile_shader(vertex_shader);
-		let fragment_shader = gl_ctx.create_shader(glow::FRAGMENT_SHADER)?;
-		gl_ctx.shader_source(fragment_shader, FRAGMENT_SHADER);
-		gl_ctx.compile_shader(fragment_shader);
-		shader_program = gl_ctx.create_program()?;
-		gl_ctx.attach_shader(shader_program, vertex_shader);
-		gl_ctx.attach_shader(shader_program, fragment_shader);
-		gl_ctx.link_program(shader_program);
-		gl_ctx.delete_shader(vertex_shader);
-		gl_ctx.delete_shader(fragment_shader);
-
-		// set up vertices and vertex attributes
-		vertex_array = gl_ctx.create_vertex_array()?;
-		let buffer = gl_ctx.create_buffer()?;
-		gl_ctx.bind_vertex_array(Some(vertex_array));
-		gl_ctx.bind_buffer(glow::ARRAY_BUFFER, Some(buffer));
-		let vertices: [f32; 9] = [-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0];
-		gl_ctx.buffer_data_u8_slice(
-			glow::ARRAY_BUFFER,
-			bytemuck::bytes_of(&vertices),
-			glow::STATIC_DRAW,
-		);
-		gl_ctx.vertex_attrib_pointer_f32(
-			0,
-			3,
-			glow::FLOAT,
-			false,
-			(3 * std::mem::size_of::<f32>()).try_into().unwrap(),
-			0,
-		);
-		gl_ctx.enable_vertex_attrib_array(0);
-		gl_ctx.bind_buffer(glow::ARRAY_BUFFER, None);
-		gl_ctx.bind_vertex_array(None);
-	}
-	'running: loop {
-		unsafe {
-			gl_ctx.clear_color(0.2, 0.3, 0.3, 1.0);
-			gl_ctx.clear(glow::COLOR_BUFFER_BIT);
-			gl_ctx.use_program(Some(shader_program));
-			gl_ctx.bind_vertex_array(Some(vertex_array));
-			gl_ctx.draw_arrays(glow::TRIANGLES, 0, 3);
-		}
-		window.gl_swap_window();
-		for event in event_pump.poll_iter() {
-			if let sdl2::event::Event::Quit { .. } = event {
-				break 'running;
-			}
-		}
-		std::thread::sleep(Duration::from_millis(2));
-	}
+	let mut ctx = Context::new()?;
+	ctx.run(Game::new()).unwrap();
 	Ok(())
 }
---
+++ src/lib.rs
@@ -0,0 +1,129 @@
+use std::{error::Error, time::Duration};
+
+use glow::{HasContext, NativeProgram};
+use sdl2::{
+	event::Event,
+	video::{GLContext, Window},
+	EventPump, Sdl, VideoSubsystem,
+};
+
+const VERTEX_SHADER: &str = include_str!("vertex.glsl");
+const FRAGMENT_SHADER: &str = include_str!("fragment.glsl");
+
+#[allow(unused_variables)]
+pub trait Game<E> {
+	fn event(&mut self, ctx: &mut Context, event: Event) -> Result<(), E> {
+		Ok(())
+	}
+
+	fn update(&mut self, ctx: &mut Context) -> Result<(), E> {
+		Ok(())
+	}
+
+	fn draw(&mut self, ctx: &mut Context) -> Result<(), E> {
+		Ok(())
+	}
+}
+
+pub struct GraphicsContext {
+	_sdl_gl_ctx: GLContext,
+	gl: glow::Context,
+	shader_program: NativeProgram,
+}
+
+impl GraphicsContext {
+	fn new(video: &VideoSubsystem, window: &Window) -> Result<Self, Box<dyn Error>> {
+		let _sdl_gl_ctx = window.gl_create_context()?;
+		let gl = unsafe {
+			glow::Context::from_loader_function(|name| video.gl_get_proc_address(name) as *const _)
+		};
+		let shader_program;
+		unsafe {
+			// set up shaders
+			let vertex_shader = gl.create_shader(glow::VERTEX_SHADER)?;
+			gl.shader_source(vertex_shader, VERTEX_SHADER);
+			gl.compile_shader(vertex_shader);
+			let fragment_shader = gl.create_shader(glow::FRAGMENT_SHADER)?;
+			gl.shader_source(fragment_shader, FRAGMENT_SHADER);
+			gl.compile_shader(fragment_shader);
+			shader_program = gl.create_program()?;
+			gl.attach_shader(shader_program, vertex_shader);
+			gl.attach_shader(shader_program, fragment_shader);
+			gl.link_program(shader_program);
+			gl.delete_shader(vertex_shader);
+			gl.delete_shader(fragment_shader);
+
+			// set up vertices and vertex attributes
+			let vertex_array = gl.create_vertex_array()?;
+			gl.bind_vertex_array(Some(vertex_array));
+			gl.vertex_attrib_pointer_f32(
+				0,
+				3,
+				glow::FLOAT,
+				false,
+				(3 * std::mem::size_of::<f32>()).try_into().unwrap(),
+				0,
+			);
+			gl.enable_vertex_attrib_array(0);
+		}
+		Ok(Self {
+			_sdl_gl_ctx,
+			gl,
+			shader_program,
+		})
+	}
+
+	pub fn clear(&self, red: f32, green: f32, blue: f32, alpha: f32) {
+		unsafe {
+			self.gl.clear_color(red, green, blue, alpha);
+			self.gl.clear(glow::COLOR_BUFFER_BIT);
+		}
+	}
+}
+
+pub struct Context {
+	_sdl: Sdl,
+	video: VideoSubsystem,
+	event_pump: EventPump,
+	window: Window,
+	graphics_ctx: GraphicsContext,
+}
+
+impl Context {
+	pub fn new() -> Result<Self, Box<dyn Error>> {
+		let sdl = sdl2::init()?;
+		let video = sdl.video()?;
+		let gl_attr = video.gl_attr();
+		gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+		gl_attr.set_context_version(3, 3);
+		let window = video.window("Window", 800, 600).opengl().build()?;
+		let graphics_ctx = GraphicsContext::new(&video, &window)?;
+		let event_pump = sdl.event_pump()?;
+		Ok(Self {
+			_sdl: sdl,
+			video,
+			event_pump,
+			window,
+			graphics_ctx,
+		})
+	}
+
+	pub fn graphics(&self) -> &GraphicsContext {
+		&self.graphics_ctx
+	}
+
+	pub fn run<E>(&mut self, mut game: impl Game<E>) -> Result<(), E> {
+		loop {
+			while let Some(event) = self.event_pump.poll_event() {
+				if let Event::Quit { .. } = event {
+					return Ok(());
+				}
+				game.event(self, event)?;
+			}
+			game.update(self)?;
+			game.draw(self)?;
+			self.window.gl_swap_window();
+			std::thread::sleep(Duration::from_millis(2));
+		}
+	}
+}

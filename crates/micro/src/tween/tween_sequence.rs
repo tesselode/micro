5b554dc20bfa652dfb85e0e837ab0c04e9c3d740
Andrew Minnich <aminnich3@gmail.com>
2023-10-28 03:07:49 -0400

    add TweenSequence::simple

--- crates/micro/src/tween/tween_sequence.rs
+++ crates/micro/src/tween/tween_sequence.rs
@@ -1,4 +1,7 @@
-use std::{ops::Range, time::Duration};
+use std::{
+	ops::{Range, RangeInclusive},
+	time::Duration,
+};
 
 use super::{Easing, Tweenable};
 
@@ -16,6 +19,12 @@ impl<T: Tweenable + Copy> TweenSequence<T> {
 			tweens: vec![],
 			elapsed: Duration::ZERO,
 		}
+	}
+
+	pub fn simple(duration: Duration, values: RangeInclusive<T>, easing: Easing) -> Self {
+		let mut tween_sequence = TweenSequence::new(*values.start());
+		tween_sequence = tween_sequence.tween(duration, *values.end(), easing);
+		tween_sequence
 	}
 
 	pub fn tween(mut self, duration: Duration, target: T, easing: Easing) -> Self {

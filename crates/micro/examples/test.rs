181bb57689164d6aa2b513f808a8ba93d098ba22
Andrew Minnich <aminnich3@gmail.com>
2024-10-05 20:38:36 -0400

    add LineSegment type with Circle intersection

--- crates/micro/examples/test.rs
+++ crates/micro/examples/test.rs
@@ -1,62 +1,77 @@
 use std::{error::Error, time::Duration};
 
-use glam::vec2;
+use glam::{vec2, Vec2};
 use micro::{
 	color::ColorConstants,
-	graphics::mesh::{Mesh, ShapeStyle},
-	math::Circle,
-	tween::{Easing, TweenSequence},
+	graphics::mesh::{MeshBuilder, ShapeStyle},
+	input::MouseButton,
+	math::{Circle, LineSegment},
 	App, Context, ContextSettings,
 };
-use palette::LinSrgb;
+use palette::{LinSrgb, LinSrgba};
+
+const CIRCLE_RADIUS: f32 = 50.0;
 
 fn main() {
 	micro::run(ContextSettings::default(), Game::new);
 }
 
 struct Game {
-	tween_sequence: TweenSequence<f32, Duration, Event>,
+	line_segment_start: Vec2,
+	circle_center: Vec2,
 }
 
 impl Game {
 	fn new(_ctx: &mut Context) -> Result<Self, Box<dyn Error>> {
 		Ok(Self {
-			tween_sequence: TweenSequence::new(100.0)
-				.tween(Duration::from_secs(1), 700.0, Easing::Linear)
-				.emit(Event::Ping)
-				.tween(Duration::from_secs(1), 100.0, Easing::Linear)
-				.emit(Event::Pong)
-				.looping(),
+			line_segment_start: Vec2::splat(50.0),
+			circle_center: vec2(400.0, 300.0),
 		})
+	}
+
+	fn circle(&self) -> Circle {
+		Circle {
+			center: self.circle_center,
+			radius: CIRCLE_RADIUS,
+		}
+	}
+
+	fn line_segment(&self, ctx: &Context) -> LineSegment {
+		LineSegment {
+			start: self.line_segment_start,
+			end: ctx.mouse_position().as_vec2(),
+		}
 	}
 }
 
 impl App<Box<dyn Error>> for Game {
-	fn update(&mut self, ctx: &mut Context, delta_time: Duration) -> Result<(), Box<dyn Error>> {
-		self.tween_sequence.update(delta_time);
-		while let Some(event) = self.tween_sequence.pop_event() {
-			dbg!(event);
+	fn update(&mut self, ctx: &mut Context, _delta_time: Duration) -> Result<(), Box<dyn Error>> {
+		if ctx.is_mouse_button_down(MouseButton::Left) {
+			self.line_segment_start = ctx.mouse_position().as_vec2();
+		}
+		if ctx.is_mouse_button_down(MouseButton::Right) {
+			self.circle_center = ctx.mouse_position().as_vec2();
 		}
 		Ok(())
 	}
 
 	fn draw(&mut self, ctx: &mut Context) -> Result<(), Box<dyn Error>> {
 		ctx.clear(LinSrgb::BLACK);
-		Mesh::circle(
-			ctx,
-			ShapeStyle::Fill,
-			Circle {
-				center: vec2(self.tween_sequence.current(), 300.0),
-				radius: 20.0,
-			},
-		)?
-		.draw(ctx);
+		let circle = self.circle();
+		let line_segment = self.line_segment(ctx);
+		MeshBuilder::new()
+			.with_circle(ShapeStyle::Stroke(2.0), circle, LinSrgb::WHITE.into())?
+			.with_simple_polyline(
+				2.0,
+				<[Vec2; 2]>::from(line_segment),
+				if line_segment.intersects_circle(circle) {
+					LinSrgba::RED
+				} else {
+					LinSrgba::WHITE
+				},
+			)?
+			.build(ctx)
+			.draw(ctx);
 		Ok(())
 	}
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-enum Event {
-	Ping,
-	Pong,
 }
---
+++ crates/micro/src/math/line_segment.rs
@@ -0,0 +1,120 @@
+use glam::{vec2, Mat4, Vec2};
+
+use super::Circle;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct LineSegment {
+	pub start: Vec2,
+	pub end: Vec2,
+}
+
+impl LineSegment {
+	pub fn midpoint(self) -> Vec2 {
+		(self.start + self.end) / 2.0
+	}
+
+	pub fn transformed(self, transform: Mat4) -> Self {
+		Self {
+			start: transform
+				.transform_point3(self.start.extend(0.0))
+				.truncate(),
+			end: transform.transform_point3(self.end.extend(0.0)).truncate(),
+		}
+	}
+
+	pub fn translated(self, translation: Vec2) -> Self {
+		Self {
+			start: self.start + translation,
+			end: self.end + translation,
+		}
+	}
+
+	pub fn translated_x(self, translation: f32) -> Self {
+		self.translated(vec2(translation, 0.0))
+	}
+
+	pub fn translated_y(self, translation: f32) -> Self {
+		self.translated(vec2(0.0, translation))
+	}
+
+	pub fn scaled(self, scale: Vec2) -> Self {
+		Self {
+			start: self.start * scale,
+			end: self.end * scale,
+		}
+	}
+
+	pub fn scaled_x(self, scale: f32) -> Self {
+		self.scaled(vec2(scale, 0.0))
+	}
+
+	pub fn scaled_y(self, scale: f32) -> Self {
+		self.scaled(vec2(0.0, scale))
+	}
+
+	pub fn rotated(self, rotation: f32) -> Self {
+		self.transformed(Mat4::from_rotation_z(rotation))
+	}
+
+	pub fn length(self) -> f32 {
+		(self.end - self.start).length()
+	}
+
+	pub fn length_squared(self) -> f32 {
+		(self.end - self.start).length_squared()
+	}
+
+	pub fn contains_point(self, point: Vec2, tolerance: f32) -> bool {
+		let distance_from_start = (point - self.start).length();
+		let distance_from_end = (point - self.end).length();
+		let distance_difference = ((distance_from_start + distance_from_end) - self.length()).abs();
+		distance_difference <= tolerance
+	}
+
+	pub fn intersects_circle(self, circle: Circle) -> bool {
+		const LINE_ON_POINT_TOLERANCE: f32 = 0.01;
+		if circle.contains_point(self.start) {
+			return true;
+		}
+		if circle.contains_point(self.end) {
+			return true;
+		}
+		let dot = (circle.center - self.start).dot(self.end - self.start) / self.length_squared();
+		let closest = self.start + dot * (self.end - self.start);
+		if !self.contains_point(closest, LINE_ON_POINT_TOLERANCE) {
+			return false;
+		}
+		let distance_squared = (closest - circle.center).length_squared();
+		distance_squared <= circle.radius.powi(2)
+	}
+}
+
+impl From<LineSegment> for [Vec2; 2] {
+	fn from(value: LineSegment) -> Self {
+		[value.start, value.end]
+	}
+}
+
+impl From<[Vec2; 2]> for LineSegment {
+	fn from(value: [Vec2; 2]) -> Self {
+		Self {
+			start: value[0],
+			end: value[1],
+		}
+	}
+}
+
+impl From<LineSegment> for (Vec2, Vec2) {
+	fn from(value: LineSegment) -> Self {
+		(value.start, value.end)
+	}
+}
+
+impl From<(Vec2, Vec2)> for LineSegment {
+	fn from(value: (Vec2, Vec2)) -> Self {
+		Self {
+			start: value.0,
+			end: value.1,
+		}
+	}
+}

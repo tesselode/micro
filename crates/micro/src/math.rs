97c232bd12175e4d2dbf363635739a055997ebca
Andrew Minnich <aminnich3@gmail.com>
2025-03-29 19:24:26 -0400

    remove VecConstants
    glam already has very similar constants

--- crates/micro/src/math.rs
+++ crates/micro/src/math.rs
@@ -9,7 +9,6 @@ mod ray;
 mod rect;
 mod to_index;
 mod urect;
-mod vec_constants;
 
 pub use cardinal_direction::*;
 pub use circle::*;
@@ -22,41 +21,5 @@ pub use ray::*;
 pub use rect::*;
 pub use to_index::*;
 pub use urect::*;
-pub use vec_constants::*;
 
 pub use glam::*;
-
-/* use lyon_tessellation::{TessellationError, VertexBuffers};
-use palette::LinSrgba;
-
-use crate::{
-	color::ColorConstants,
-	graphics::{
-		Vertex2d,
-		mesh::{MeshBuilder, ShapeStyle},
-	},
-};
-
-pub fn triangulate_polygon(points: &[Vec2]) -> Result<Vec<Vec<Vec2>>, TessellationError> {
-	let mesh_builder = MeshBuilder::new().with_simple_polygon(
-		ShapeStyle::Fill,
-		points.iter().copied(),
-		LinSrgba::WHITE,
-	)?;
-	let buffers = &mesh_builder.buffers;
-	Ok(buffers
-		.indices
-		.chunks_exact(3)
-		.map(|indices| triangle_points(indices, buffers))
-		.collect())
-}
-
-fn triangle_points(indices: &[u32], buffers: &VertexBuffers<Vertex2d, u32>) -> Vec<Vec2> {
-	indices
-		.iter()
-		.map(|index| {
-			let position = buffers.vertices[*index as usize].position;
-			Vec2::new(position.x, position.y)
-		})
-		.collect()
-} */

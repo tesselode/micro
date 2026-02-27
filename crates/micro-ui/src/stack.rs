45c97fdacf5167fcc55d278ba7ef83c7ca491fa1
Andrew Minnich <aminnich3@gmail.com>
2026-02-27 04:15:53 -0500

    update stack api to be more fluent

--- crates/micro-ui/src/stack.rs
+++ crates/micro-ui/src/stack.rs
@@ -12,27 +12,51 @@ use super::{AxisSizing, LayoutResult, Widget, WidgetMouseState};
 #[derive(Debug)]
 pub struct Stack {
 	direction: Axis,
-	settings: StackSettings,
+	gap: f32,
+	cross_align: f32,
+	cross_sizing: AxisSizing,
 	children: Vec<Box<dyn Widget>>,
 	mouse_state: Option<WidgetMouseState>,
 }
 
 impl Stack {
-	pub fn horizontal(settings: StackSettings) -> Self {
+	pub fn horizontal() -> Self {
 		Self {
 			direction: Axis::Horizontal,
-			settings,
+			gap: 0.0,
+			cross_align: 0.0,
+			cross_sizing: AxisSizing::Shrink,
 			children: vec![],
 			mouse_state: None,
 		}
 	}
 
-	pub fn vertical(settings: StackSettings) -> Self {
+	pub fn vertical() -> Self {
 		Self {
 			direction: Axis::Vertical,
-			settings,
+			gap: 0.0,
+			cross_align: 0.0,
+			cross_sizing: AxisSizing::Shrink,
 			children: vec![],
 			mouse_state: None,
+		}
+	}
+
+	pub fn gap(self, gap: f32) -> Self {
+		Self { gap, ..self }
+	}
+
+	pub fn cross_align(self, cross_align: f32) -> Self {
+		Self {
+			cross_align,
+			..self
+		}
+	}
+
+	pub fn cross_sizing(self, cross_sizing: AxisSizing) -> Self {
+		Self {
+			cross_sizing,
+			..self
 		}
 	}
 
@@ -66,17 +90,15 @@ impl Widget for Stack {
 	) -> Vec2 {
 		let _span = tracy_client::span!();
 		let total_child_main_axis_size =
-			total_child_main_axis_size(self.direction, previous_child_sizes, self.settings.gap);
+			total_child_main_axis_size(self.direction, previous_child_sizes, self.gap);
 		match self.direction {
 			Axis::Horizontal => vec2(
 				allotted_size_from_parent.x - total_child_main_axis_size,
-				self.settings
-					.cross_sizing
+				self.cross_sizing
 					.allotted_size_for_children(allotted_size_from_parent.y),
 			),
 			Axis::Vertical => vec2(
-				self.settings
-					.cross_sizing
+				self.cross_sizing
 					.allotted_size_for_children(allotted_size_from_parent.x),
 				allotted_size_from_parent.y - total_child_main_axis_size,
 			),
@@ -93,8 +115,8 @@ impl Widget for Stack {
 		match self.direction {
 			Axis::Horizontal => {
 				let parent_size = vec2(
-					total_child_main_axis_size(self.direction, child_sizes, self.settings.gap),
-					self.settings.cross_sizing.final_parent_size(
+					total_child_main_axis_size(self.direction, child_sizes, self.gap),
+					self.cross_sizing.final_parent_size(
 						allotted_size_from_parent.y,
 						child_sizes.iter().map(|size| size.y),
 					),
@@ -104,8 +126,8 @@ impl Widget for Stack {
 					.iter()
 					.map(|size| {
 						let x = next_child_x;
-						next_child_x += size.x + self.settings.gap;
-						vec2(x, (parent_size.y - size.y) * self.settings.cross_align)
+						next_child_x += size.x + self.gap;
+						vec2(x, (parent_size.y - size.y) * self.cross_align)
 					})
 					.collect();
 				LayoutResult {
@@ -115,19 +137,19 @@ impl Widget for Stack {
 			}
 			Axis::Vertical => {
 				let parent_size = vec2(
-					self.settings.cross_sizing.final_parent_size(
+					self.cross_sizing.final_parent_size(
 						allotted_size_from_parent.x,
 						child_sizes.iter().map(|size| size.x),
 					),
-					total_child_main_axis_size(self.direction, child_sizes, self.settings.gap),
+					total_child_main_axis_size(self.direction, child_sizes, self.gap),
 				);
 				let mut next_child_y = 0.0;
 				let child_positions = child_sizes
 					.iter()
 					.map(|size| {
 						let y = next_child_y;
-						next_child_y += size.y + self.settings.gap;
-						vec2((parent_size.x - size.x) * self.settings.cross_align, y)
+						next_child_y += size.y + self.gap;
+						vec2((parent_size.x - size.x) * self.cross_align, y)
 					})
 					.collect();
 				LayoutResult {
@@ -137,13 +159,6 @@ impl Widget for Stack {
 			}
 		}
 	}
-}
-
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub struct StackSettings {
-	pub gap: f32,
-	pub cross_align: f32,
-	pub cross_sizing: AxisSizing,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]

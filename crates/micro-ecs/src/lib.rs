c9d3cbbf57f8819a276ab1596e9a3097269090f4
Andrew Minnich <aminnich3@gmail.com>
2026-09-21 01:53:58 -0400

    resources helper

--- crates/micro-ecs/src/lib.rs
+++ crates/micro-ecs/src/lib.rs
@@ -1,12 +1,15 @@
 mod ecs;
 mod event_dispatcher;
+mod has_resources;
 mod queues;
 mod systems;
 
 pub use ecs::*;
+pub use has_resources::*;
 pub use hecs::*;
-use micro::Micro;
 pub use queues::*;
+
+use micro::Micro;
 
 pub type System<Globals, Event> =
 	fn(&mut Micro, &mut Globals, &mut World, &mut Queues<Globals>, &Event);
